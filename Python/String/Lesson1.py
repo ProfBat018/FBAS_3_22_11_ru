d35426e180d2244141bc3d58b29187387777934a
Elvin <elvin.azim01@gmail.com>
2022-12-29 16:47:00 +0400

    List and string lesson 1

---
+++ Python/String/Lesson1.py
@@ -0,0 +1,44 @@
+# num = 5
+#
+# a = str(num)
+# print(a)
+
+# name = "elvin_123"
+# print(name.count("E"))
+
+# print(name.upper())
+# print(name.lower())
+# print(name.capitalize())
+# print(name.title())
+
+# print(name.isalpha())   # is alphabet
+# print(name.isalnum())   # is alphanumeric
+
+# print(chr(65))
+# print(ord('A'))
+
+# print(name.isascii())
+
+# if not name.isalnum():
+#     print("This is symbol")
+
+
+# check = name.islower()
+# print(check)
+
+# print(name.casefold()) # the same thing as lower
+
+# print(name.isdigit())
+# print(name.isnumeric())
+# print(name.isdecimal())
+
+
+data = "Elvin Azimov 21"
+
+person = data.split()
+
+print(person)
+
+
+
+
