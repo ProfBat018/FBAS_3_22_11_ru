b7d60784e5399f5d8bab78512cd3c3255e60f857
Elvin <elvin.azim01@gmail.com>
2023-02-14 17:07:01 +0400

    Loop and random

--- C++/While/While/While.cpp
+++ C++/While/While/While.cpp
@@ -1,9 +1,50 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
 int main()
 {
+	// Цикл с предусловием
+
+	/*
+	int i = 0;
+
+	while (i <= 10)
+	{
+		cout << ++i << endl;
+	}
+	*/
+
+	// Цикл с постусловием
+
+
+	/*int i = 0;
+
+	do
+	{
+		cout << ++i;
+	} while (i == 1);*/
+
+
+	// for - цикл с параметрами
+	
+	/*
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		cout << i << endl;
+	}
+	cout << i;
+	*/
+
+	//for (; ;)
+	//{
+	//	cout << "Hello";
+	//}
+
+
+
 
 
 	return 0;
