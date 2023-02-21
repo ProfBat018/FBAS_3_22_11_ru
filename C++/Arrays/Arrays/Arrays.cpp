d54c239a653d72a1cd198056ef5b54937bbcd405
Elvin <elvin.azim01@gmail.com>
2023-02-21 17:06:23 +0400

    multi array

---
+++ C++/Arrays/Arrays/Arrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Arrays
+	
+	//int[] arr;
+	
+	/*
+	const int length = 10;
+
+	int arr[length]{};
+
+
+	for (size_t i = 0; i < length; i++)
+	{
+		cout << arr + i << '\n';
+	}
+	*/
+
+
+
+
+	return 0;
+}
---
+++ C++/Pointers/Pointers/Pointers.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	int number = 5;
+	cout << number << endl;  // Value
+	cout << &number << endl; // Address
+	cout << *(&number) << endl; // Dereference address
+	*/
+
+	/*
+		Указатель - это переменная, которая хранит в себе адрес.
+		Указатель весит 4 или 8 байт в зависимости от битности системы.
+		х86 и х64 непосредственно.
+		* после типа данных объявляет укзатель. 
+		* до названия переменной разименовывает его.
+		& - Амперсанд, возвращает адрес переменной.
+	*/
+
+	//int num = 1;
+	//int* ptrNum = &num; // Запиши в указатель адрес переменной num
+
+
+	//cout << "num" << '\t' << num << endl;
+	//cout << "&num" << '\t' << & num << endl;
+	//cout << "ptrNum" << '\t' << ptrNum << endl;
+	//cout << "*ptrNum" << '\t' << *ptrNum << endl;
+
+	//*ptrNum = 5;
+	//cout << num;
+
+
+	//char a = 'a';
+	//char* ptrA = &a;
+
+	//cout << "char size: " << sizeof(char) << endl;
+	//cout << "char pointer size: " << sizeof(ptrA) << endl;
+
+
+
+	return 0;
+}
