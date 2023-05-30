18821b13556a972c30a79717556005cb0580f4f4
Elvin Azimov <elvin.azim01@gmail.com>
2023-05-30 16:43:36 +0400

    Polymorphism lesson 1

--- OOP/csharpexample/csharpexample/Program.cs
+++ OOP/csharpexample/csharpexample/Program.cs
@@ -1,24 +1,7 @@
-﻿class Transport
-{
-    public string make;
-    public string model;
-    public int speed;
-
-    public Transport(string make, string model, int speed)
-    {
-        this.make = make;
-        this.model = model;
-        this.speed = speed;
-    }
-}
+﻿using System.Collections;
+using System.Collections.Generic;
 
 
-class Car : Transport
-{
-    public string color;
 
-    public Car(string make, string model, int speed, string color) : base(make, model, speed)
-    {
-        this.color = color;
-    }
-}
+
+
