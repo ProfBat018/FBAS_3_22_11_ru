58a57eb17b9fa9530c7a9b82ccb651a943961baf
Elvin Azimov <elvin.azim01@gmail.com>
2023-08-24 16:15:33 +0400

    Lesson2

--- OOP/STL/globals.h
+++
@@ -1,7 +0,0 @@
-#include <regex>
-
-namespace globals {
-    std::regex email("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$");
-    std::regex phone("^\\+?[0-9]{3}-?[0-9]{6,12}$");
-    std::regex date("^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$");
-}
