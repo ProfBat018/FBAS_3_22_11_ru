0e52d46ccce7f1263bf92ecb5d9d6ad1a40bd75a
Elvin Azimov <elvin.azim01@gmail.com>
2023-09-14 17:07:49 +0400

    Delegates

---
+++ Delegates/Delegates/Tax.cs
@@ -0,0 +1,8 @@
+﻿namespace Delegates;
+
+public class Tax
+{
+    public string Name { get; set; } // имя налога
+    public decimal Rate { get; set; } // ставка налога
+    public Func<decimal, decimal> TaxDelegate { get; set; } // делегат налога
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿// string name = "Elvin";
+//
+// name.Foo(); // syntax sugar 
+// StringExtensions.Foo(name);
+//
+// static class StringExtensions {
+//     public static void Foo(this string data)
+//     {
+//         Console.WriteLine($"Aloha: {data}");
+//     }
+// }
+
+List<int> nums = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+// LINQ Method
+// nums.Where(x => x % 2 == 0).ToList().ForEach(x => Console.WriteLine(x));
+
+var res = nums.Where(x => x % 2 == 0);
+
+List<int> evenNums = new(res);
+Stack<int> evenNumsStack = new(res);
+Queue<int> evenNumsQueue = new(res);
+
+
+
+
