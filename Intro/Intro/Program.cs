533184d96e5f5e6c4465302d90c0f7a2b2928614
WAYNE <wayne@WAYNEs-MacBook-Pro.local>
2024-01-09 17:47:34 +0400

    Updated

--- Intro/Intro/Program.cs
+++ Intro/Intro/Program.cs
@@ -12,19 +12,25 @@ var connectionString = config.GetConnectionString("Default");
 
 using SqlConnection conn = new(connectionString);
 
+SqlCommand command = new("select * from People", conn);
+
 conn.Open();
 
-using SqlCommand command = new("select * from People", conn);
-
-var reader = command.ExecuteReader(); // ExecuteReader - это метод,
-                                      // который использутеся для чтения
-                                      // из таблицы, то есть select 
-
-while (reader.Read())
 {
-    var id = reader.GetInt32(0);
-    Console.WriteLine($"{reader[0]}\t{reader[1]}");
+    using SqlDataReader reader = command.ExecuteReader(); // ExecuteReader - это метод,
+    // который использутеся для чтения
+    // из таблицы, то есть select 
 
-    // Console.WriteLine($"{reader[0]}\t{reader[1]}");
+    while (reader.Read())
+    {
+        var id = reader.GetInt32(0);
+        Console.WriteLine($"{reader[0]}\t{reader[1]}");
+    }
 }
+
+command = new SqlCommand("insert into People(Name, Surname, Age) values(N'Baxa', N'Mirzayev', 17);", conn);
+
+var res = command.ExecuteNonQuery();
+
+Console.WriteLine(res);
 
