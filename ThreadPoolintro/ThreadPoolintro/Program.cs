b9b09c037b33487860482c7815aeed3eb068d95f
Azimov Elvin Rasul <azimove@itstep.edu.az>
2024-02-25 15:22:40 +0400

    Updated

---
+++ ThreadPoolintro/ThreadPoolintro/Program.cs
@@ -0,0 +1,35 @@
+﻿
+class Program
+{
+    public static void ThreadPoolExample()
+    {
+        Console.WriteLine($"Thread count: {ThreadPool.ThreadCount}");
+        Console.WriteLine($"Pending Thread count: {ThreadPool.PendingWorkItemCount}");
+        Console.WriteLine($"Completed Thread count: {ThreadPool.CompletedWorkItemCount}");
+    }
+
+    public static void Main()
+    {
+        using var countDownEvent = new CountdownEvent(3);
+
+        var mutex = new Mutex();
+
+        for (int i = 0; i < countDownEvent.InitialCount; i++)
+        {
+            ThreadPool.QueueUserWorkItem(
+            (p) =>
+            {
+                mutex.WaitOne();
+                Console.WriteLine($"Is ThreadPool Thread {Thread.CurrentThread.IsThreadPoolThread}");
+                Console.WriteLine($"Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+
+                countDownEvent.Signal();
+
+                mutex.ReleaseMutex();
+            });
+        }
+        countDownEvent.Wait();
+
+        Console.WriteLine("End of main");
+    }
+}
